66d68daf51096d3e13bcfe1153c762eec9dbdb76
LN27100 <heloceinlovehotmail.fr>
2024-02-07 07:20:51 +0100

    ajout boutton déconnexion

--- views/view-home.php
+++ views/view-home.php
@@ -20,188 +20,196 @@
         <div class="navbar-fixed">
             <nav class="navbar #e0f7fa cyan lighten-5">
                 <div class="nav-wrapper"><a href="#" class="brand-logo #e0f7fa cyan lighten-5">Portail entreprise</a>
+
                 </div>
+
             </nav>
         </div>
+
+
+
         <ul id="sidenav-left" class="sidenav sidenav-fixed #e0f7fa cyan lighten-5">
+            <?php
+
+            // Bouton de déconnexion
+            echo '<a href="../controllers/controller-signout.php" class="buttonHome2">Deconnexion</a>';
+            ?>
+
             <li><a href="../controllers/controller-home.php" class="logo-container"><?= $nom ?></a></li>
             <div class="container3">
-        <div class="profile-image-container">
-         
-                <img src="<?= $img ?>" alt="photo de profil" class="profile-image">
 
-            <form method="post" action="../controllers/controller-home.php" enctype="multipart/form-data" class="file-input-container">
-                <input type="file" name="profile_image" id="profile_image" accept="image/png, image/gif, image/jpeg, image/jpg" required>
-                <input type="submit" value="Télécharger">
-            </form>
-        </div>
+                <div class="profile-image-container">
 
-        <div class="profile-info">
-            <p><span class="styleProfil"> Nom:</span> <?= $nom ?></p>
-            <p><span class="styleProfil">Siret: </span> <?= $siret ?></p>
-            <p><span class="styleProfil">Email: </span> <?= $email ?></p>
-            <p><span class="styleProfil">Adresse: </span> <?= $adresse ?></p>
-            <p><span class="styleProfil">Code postal: </span> <?= $code_postal ?></p>
-            <p><span class="styleProfil">Ville: </span> <?= $ville ?></p>
+                    <img src="<?= $img ?>" alt="photo de profil" class="profile-image">
 
-        </div>
+                    <form method="post" action="../controllers/controller-home.php" enctype="multipart/form-data" class="file-input-container">
+                        <input type="file" name="profile_image" id="profile_image" accept="image/png, image/gif, image/jpeg, image/jpg" required>
+                        <input type="submit" value="Télécharger">
+                    </form>
+                </div>
 
+                <div class="profile-info">
+                    <p><span class="styleProfil"> Nom:</span> <?= $nom ?></p>
+                    <p><span class="styleProfil">Siret: </span> <?= $siret ?></p>
+                    <p><span class="styleProfil">Email: </span> <?= $email ?></p>
+                    <p><span class="styleProfil">Adresse: </span> <?= $adresse ?></p>
+                    <p><span class="styleProfil">Code postal: </span> <?= $code_postal ?></p>
+                    <p><span class="styleProfil">Ville: </span> <?= $ville ?></p>
 
-        <div class="contnair">
-            <button id="editDescriptionBtn">Modifier le profil</button>
-
-            <form action="../controllers/controller-profil.php" method="post" class="deleteProfil">
-                <input type="hidden" name="delete_profile" value="<?= $enterprise_id ?>">
-                <button class="delete_profile" type="submit" name="delete_profile" onclick="return confirm('Voulez-vous vraiment supprimer ce profil ?')">Supprimer le profil</button>
-            </form>
-        </div>
-    </div>
-
-    </div>
+                </div>
 
 
-    <!-- Formulaire de modification du profil (masqué par défaut) -->
-    <form method="post" action="../controllers/controller-profil.php" class="transparent-form" enctype="multipart/form-data" id="editDescriptionForm" style="display: none;">
+                <div class="contnair">
+                    <button id="editDescriptionBtn">Modifier le profil</button>
 
-    
-
-        <div class="profile-info">
-            <p><span class="styleProfil"> Nom:</span></p>
-            <input type="text" name="enterprise_name" placeholder="Nouveau nom" value="<?= $nom ?>">
-
-            <!-- Affichage des erreurs pour le nom -->
-            <?php if (isset($errors['enterprise_name'])) { ?>
-                <span class="error-message"><?= $errors['enterprise_name']; ?></span>
-            <?php } ?>
-
-
-            <p><span class="styleProfil">Email:</span></p>
-            <input type="text" name="enterprise_email" placeholder="Nouveau email" value="<?= $email ?>">
-
-            <!-- Affichage des erreurs pour l'email -->
-            <?php if (isset($errors['enterprise_email'])) { ?>
-                <span class="error-message"><?= $errors['enterprise_email']; ?></span>
-            <?php } ?>
-
-            <p><span class="styleProfil"> Adresse:</span></p>
-            <input type="text" name="enterprise_adress" placeholder="Nouveau nom" value="<?= $adresse ?>">
-
-            <!-- Affichage des erreurs pour l'adresse -->
-            <?php if (isset($errors['enterprise_adress'])) { ?>
-                <span class="error-message"><?= $errors['enterprise_adress']; ?></span>
-            <?php } ?>
-
-            <p><span class="styleProfil"> Code postal:</span></p>
-            <input type="text" name="enterprise_zipcode" placeholder="Nouveau nom" value="<?= $code_postal ?>">
-
-            <!-- Affichage des erreurs pour le code postal -->
-            <?php if (isset($errors['enterprise_zipcode'])) { ?>
-                <span class="error-message"><?= $errors['enterprise_zipcode']; ?></span>
-            <?php } ?>
-
-            <p><span class="styleProfil"> Ville:</span></p>
-            <input type="text" name="enterprise_city" placeholder="Nouveau nom" value="<?= $ville ?>">
-
-            <!-- Affichage des erreurs pour le code postal -->
-            <?php if (isset($errors['enterprise_city'])) { ?>
-                <span class="error-message"><?= $errors['enterprise_city']; ?></span>
-            <?php } ?>
-
-
-
-            <div class="profile-info">
-                <input type="submit" name="save_modification" value="Enregistrer" class="file-input-button">
-                <button type="button" id="cancelEditBtn" class="file-input-button">Annuler</button>
+                    <form action="../controllers/controller-profil.php" method="post" class="deleteProfil">
+                        <input type="hidden" name="delete_profile" value="<?= $enterprise_id ?>">
+                        <button class="delete_profile" type="submit" name="delete_profile" onclick="return confirm('Voulez-vous vraiment supprimer ce profil ?')">Supprimer le profil</button>
+                    </form>
+                </div>
             </div>
-    </form>
 
+            </div>
+
+
+            <!-- Formulaire de modification du profil (masqué par défaut) -->
+            <form method="post" action="../controllers/controller-profil.php" class="transparent-form" enctype="multipart/form-data" id="editDescriptionForm" style="display: none;">
+
+                <div class="profile-info">
+                    <p><span class="styleProfil"> Nom:</span></p>
+                    <input type="text" name="enterprise_name" placeholder="Nouveau nom" value="<?= $nom ?>">
+
+                    <!-- Affichage des erreurs pour le nom -->
+                    <?php if (isset($errors['enterprise_name'])) { ?>
+                        <span class="error-message"><?= $errors['enterprise_name']; ?></span>
+                    <?php } ?>
+
+
+                    <p><span class="styleProfil">Email:</span></p>
+                    <input type="text" name="enterprise_email" placeholder="Nouveau email" value="<?= $email ?>">
+
+                    <!-- Affichage des erreurs pour l'email -->
+                    <?php if (isset($errors['enterprise_email'])) { ?>
+                        <span class="error-message"><?= $errors['enterprise_email']; ?></span>
+                    <?php } ?>
+
+                    <p><span class="styleProfil"> Adresse:</span></p>
+                    <input type="text" name="enterprise_adress" placeholder="Nouveau nom" value="<?= $adresse ?>">
+
+                    <!-- Affichage des erreurs pour l'adresse -->
+                    <?php if (isset($errors['enterprise_adress'])) { ?>
+                        <span class="error-message"><?= $errors['enterprise_adress']; ?></span>
+                    <?php } ?>
+
+                    <p><span class="styleProfil"> Code postal:</span></p>
+                    <input type="text" name="enterprise_zipcode" placeholder="Nouveau nom" value="<?= $code_postal ?>">
+
+                    <!-- Affichage des erreurs pour le code postal -->
+                    <?php if (isset($errors['enterprise_zipcode'])) { ?>
+                        <span class="error-message"><?= $errors['enterprise_zipcode']; ?></span>
+                    <?php } ?>
+
+                    <p><span class="styleProfil"> Ville:</span></p>
+                    <input type="text" name="enterprise_city" placeholder="Nouveau nom" value="<?= $ville ?>">
+
+                    <!-- Affichage des erreurs pour le code postal -->
+                    <?php if (isset($errors['enterprise_city'])) { ?>
+                        <span class="error-message"><?= $errors['enterprise_city']; ?></span>
+                    <?php } ?>
+
+                    <div class="profile-info">
+                        <input type="submit" name="save_modification" value="Enregistrer" class="file-input-button">
+                        <button type="button" id="cancelEditBtn" class="file-input-button">Annuler</button>
+                    </div>
+            </form>
     </header>
 
+
     <main>
-    <div class="container">
-        <div class="masonry row">
-            <div class="col s12">
-                <h2>Dashboard <?= $nom ?></h2>
-            </div>
-
-            <div class="row">
-                <div class="col l3 m6 s12">
-                    <div class="card #01579b light-blue darken-4">
-                        <div class="card-content white-text">
-                            <span class="card-title">Total des utilisateurs</span>
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col l3 m6 s12">
-                    <div class="card #01579b light-blue darken-4">
-                        <div class="card-content white-text">
-                            <span class="card-title">Total des utilisateurs actifs</span>
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col l3 m6 s12">
-                    <div class="card #01579b light-blue darken-4">
-                        <div class="card-content white-text">
-                            <span class="card-title">Total des trajets</span>
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col l3 m6 s12">
-                    <div class="card #01579b light-blue darken-4">
-                        <div class="card-stacked white-text">
-                            <div class="card-metric">
-                                <span class="card-title">5 derniers utilisateurs</span>
-                                <div class="card-metric-value">0.24%</div>
-                                <div class="card-metric-value">0.24%</div>
-                                <div class="card-metric-value">0.24%</div>
-                                <div class="card-metric-value">0.24%</div>
-                                <div class="card-metric-value">0.24%</div>
-                            </div>
-                            <canvas id="flush-area-chart-green" height="400px"></canvas>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col l4 m6 s12">
-                    <div class="card #01579b light-blue darken-4">
-                        <div class="card-content white-text">
-                            <span class="card-title">Stats hebdo</span>
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col l4 m6 s12">
-                    <div class="card #01579b light-blue darken-4">
-                        <div class="card-content white-text">
-                            <span class="card-title">Stats des Moyens de transport</span>
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            
-
-            <div class="row">
+        <div class="container">
+            <div class="masonry row">
                 <div class="col s12">
-                    <div class="card #01579b light-blue darken-4">
-                        <div class="card-content white-text">
-                            <span class="card-title">Total des trajets</span>
-                            <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                    <h2>Dashboard <?= $nom ?></h2>
+                </div>
+
+                <div class="row">
+                    <div class="col l3 m6 s12">
+                        <div class="card #01579b light-blue darken-4">
+                            <div class="card-content white-text">
+                                <span class="card-title">Total des utilisateurs</span>
+                                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col l3 m6 s12">
+                        <div class="card #01579b light-blue darken-4">
+                            <div class="card-content white-text">
+                                <span class="card-title">Total des utilisateurs actifs</span>
+                                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col l3 m6 s12">
+                        <div class="card #01579b light-blue darken-4">
+                            <div class="card-content white-text">
+                                <span class="card-title">Total des trajets</span>
+                                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col l3 m6 s12">
+                        <div class="card #01579b light-blue darken-4">
+                            <div class="card-stacked white-text">
+                                <div class="card-metric">
+                                    <span class="card-title">5 derniers utilisateurs</span>
+                                    <div class="card-metric-value">0.24%</div>
+                                    <div class="card-metric-value">0.24%</div>
+                                    <div class="card-metric-value">0.24%</div>
+                                    <div class="card-metric-value">0.24%</div>
+                                    <div class="card-metric-value">0.24%</div>
+                                </div>
+                                <canvas id="flush-area-chart-green" height="400px"></canvas>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col l4 m6 s12">
+                        <div class="card #01579b light-blue darken-4">
+                            <div class="card-content white-text">
+                                <span class="card-title">Stats hebdo</span>
+                                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col l4 m6 s12">
+                        <div class="card #01579b light-blue darken-4">
+                            <div class="card-content white-text">
+                                <span class="card-title">Stats des Moyens de transport</span>
+                                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="row">
+                    <div class="col s12">
+                        <div class="card #01579b light-blue darken-4">
+                            <div class="card-content white-text">
+                                <span class="card-title">Total des trajets</span>
+                                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 
 
 
