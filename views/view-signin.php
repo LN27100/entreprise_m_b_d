3796dd680d8a740a5afe2aac85568bd2f5fa377f
LN27100 <heloceinlovehotmail.fr>
2024-02-12 10:11:51 +0100

    récup placeholder signin et signup

--- views/view-signin.php
+++ views/view-signin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,52 +9,41 @@
     <link rel="stylesheet" href="../assets/css/style.css">
     <title>Connexion pro</title>
 </head>
+
 <body class="#e0f7fa cyan lighten-5">
     <?php
     ?>
     <h1 class="center-align indigo-text">Portail entreprise</h1>
     <h2 class="center-align">Veuillez vous connecter</h2>
-    
+
     <div class="container">
         <form class="row #455a64 blue-grey darken-2" method="POST" action="../controllers/controller-signin.php" novalidate>
             <div class="input-field col s12 m6 offset-m3 center-align">
-                <input id="enterprise_email" type="email" class="validate <?php if (isset($errors['enterprise_email'])) echo 'invalid'; ?>" name="enterprise_email" placeholder="adresse email" value="<?= isset($_POST['enterprise_email']) ? htmlspecialchars($_POST['enterprise_email']) : '' ?>" required>
-                <label for="enterprise_email" class="active cyan-text text-lighten-5">Email :</label>
+                <input id="enterprise_email" type="email" placeholder="saisissez votre adresse mail" class="validate <?php if (isset($errors['enterprise_email'])) echo 'invalid'; ?>" name="enterprise_email" value="<?= isset($_POST['enterprise_email']) ? htmlspecialchars($_POST['enterprise_email']) : '' ?>" required> <label for="enterprise_email" class="active cyan-text text-lighten-5">Email :</label>
                 <span class="helper-text" data-error="<?php echo isset($errors['enterprise_email']) ? $errors['enterprise_email'] : 'Champ obligatoire'; ?>"></span>
             </div>
             <div class="input-field col s12 m6 offset-m3 center-align">
-                <input id="enterprise_password" type="password" class="validate <?php if (isset($errors['enterprise_password'])) echo 'invalid'; ?>" name="enterprise_password" placeholder="Votre mot de passe" aria-label="password" aria-describedby="password">
+                <input id="enterprise_password"  placeholder="saisissez votre mot de passe" type="password" class="validate <?php if (isset($errors['enterprise_password'])) echo 'invalid'; ?>" name="enterprise_password" placeholder="Votre mot de passe" aria-label="password" aria-describedby="password">
                 <label for="enterprise_password" class="cyan-text text-lighten-5">Mot de passe :</label>
                 <i class="material-icons suffix cyan-text text-lighten-5" style="cursor:pointer; position: absolute; right: 10px; top: 50%; transform: translateY(-50%);" onclick="togglePasswordVisibility()">remove_red_eye</i>
                 <span class="helper-text" data-error="<?php echo isset($errors['enterprise_password']) ? $errors['enterprise_password'] : 'Champ obligatoire'; ?>"></span>
             </div>
             <div class="input-field col s12 center-align">
-            <button class="btn waves-effect custom-btn" type="submit" id="submitButton">Se connecter</button>
+                <button class="btn waves-effect custom-btn" type="submit" id="submitButton">Se connecter</button>
             </div>
 
         </form>
 
         <div class="row">
-        <div class="custom-div center-align #01579b light-blue darken-4">
-            <p class="textBold blue-grey-text darken-3">Pas encore membre?</p>
-            <a href="../controllers/controller-signup.php" class="textBold green-text">Inscrivez-vous!</a>
+            <div class="custom-div center-align #01579b light-blue darken-4">
+                <p class="textBold blue-grey-text darken-3">Pas encore membre?</p>
+                <a href="../controllers/controller-signup.php" class="textBold green-text">Inscrivez-vous!</a>
+            </div>
         </div>
-    </div>
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
-
-    
-          
-            
-    
-
-          
-          Expand Down
-    
-    
-  
         function togglePasswordVisibility() {
             var passwordInput = document.getElementById('enterprise_password');
             if (passwordInput.type === 'password') {
@@ -64,4 +54,5 @@
         }
     </script>
 </body>
+
 </html>
--- views/view-signup.php
+++ views/view-signup.php
@@ -56,7 +56,7 @@
 
                     <label for="validationServer01" class="active cyan-text text-lighten-5">Nom d'entreprise :</label>
 
-                    <input type="text" class="form-control <?php if (isset($errors['enterprise_name'])) echo 'is-invalid'; ?>" id="validationServer01" name="enterprise_name" placeholder="ex.Afpa" value="<?= isset($_POST['enterprise_name']) ? htmlspecialchars($_POST['enterprise_name']) : '' ?>" required>
+                    <input type="text" placeholder="saisissez le nom de votre entreprise" class="form-control <?php if (isset($errors['enterprise_name'])) echo 'is-invalid'; ?>" id="validationServer01" name="enterprise_name" value="<?= isset($_POST['enterprise_name']) ? htmlspecialchars($_POST['enterprise_name']) : '' ?>" required>
 
                     <div class="invalid-feedback" id="enterpriseNameValidationFeedback">
 
@@ -78,7 +78,7 @@
 
                     <label for="validationServer03" class="cyan-text text-lighten-5">Numéro de Siret :</label>
 
-                    <input type="text" class="form-control <?php if (isset($errors['enterprise_siret'])) echo 'is-invalid'; ?>" id="validationServer03" name="enterprise_siret" placeholder="numéro siret" value="<?= isset($_POST['enterprise_siret']) ? htmlspecialchars($_POST['enterprise_siret']) : '' ?>" required>
+                    <input type="text" class="form-control <?php if (isset($errors['enterprise_siret'])) echo 'is-invalid'; ?>" id="validationServer03" name="enterprise_siret" placeholder="saisissez le siret de votre entreprise" value="<?= isset($_POST['enterprise_siret']) ? htmlspecialchars($_POST['enterprise_siret']) : '' ?>" required>
 
                     <div class="invalid-feedback" id="enterpriseSiretValidationFeedback">
 
@@ -101,7 +101,7 @@
 
                     <label for="enterprise_adress" class="cyan-text text-lighten-5">Adresse entreprise:</label>
 
-                    <input type="text" name="enterprise_adress" value="<?= isset($_POST['enterprise_adress']) ? htmlspecialchars($_POST['enterprise_adress']) : '' ?>" class="form-control <?php if (isset($errors['enterprise_adress'])) echo 'is-invalid'; ?>" required>
+                    <input type="text" placeholder="saisissez votre adresse" name="enterprise_adress" value="<?= isset($_POST['enterprise_adress']) ? htmlspecialchars($_POST['enterprise_adress']) : '' ?>" class="form-control <?php if (isset($errors['enterprise_adress'])) echo 'is-invalid'; ?>" required>
 
                     <div class="invalid-feedback" id="enterpriseAdressValidationFeedback">
 
@@ -122,7 +122,7 @@
 
                     <label for="enterprise_zipcode" class="cyan-text text-lighten-5">Code postal:</label>
 
-                    <input type="text" name="enterprise_zipcode" value="<?= isset($_POST['enterprise_zipcode']) ? htmlspecialchars($_POST['enterprise_zipcode']) : '' ?>" class="form-control <?php if (isset($errors['enterprise_zipcode'])) echo 'is-invalid'; ?>" required>
+                    <input type="text" placeholder="saisissez votre code postal" name="enterprise_zipcode" value="<?= isset($_POST['enterprise_zipcode']) ? htmlspecialchars($_POST['enterprise_zipcode']) : '' ?>" class="form-control <?php if (isset($errors['enterprise_zipcode'])) echo 'is-invalid'; ?>" required>
 
                     <div class="invalid-feedback" id="dateValidationFeedback">
 
@@ -145,7 +145,7 @@
 
                     <label for="enterprise_city" class="cyan-text text-lighten-5">Ville:</label>
 
-                    <input type="text" name="enterprise_city" value="<?= isset($_POST['enterprise_city']) ? htmlspecialchars($_POST['enterprise_city']) : '' ?>" class="form-control <?php if (isset($errors['enterprise_city'])) echo 'is-invalid'; ?>" required>
+                    <input type="text" placeholder="saisissez votre ville" name="enterprise_city" value="<?= isset($_POST['enterprise_city']) ? htmlspecialchars($_POST['enterprise_city']) : '' ?>" class="form-control <?php if (isset($errors['enterprise_city'])) echo 'is-invalid'; ?>" required>
 
                     <div class="invalid-feedback" id="enterpriseCityValidationFeedback">
 
@@ -168,7 +168,7 @@
 
                         <label for="email" class="cyan-text text-lighten-5">Email: </label>
 
-                        <input type="email" class="form-control <?php if (isset($errors['enterprise_email'])) echo 'is-invalid'; ?>" id="email" name="enterprise_email" placeholder="adresse email" value="<?= isset($_POST['enterprise_email']) ? htmlspecialchars($_POST['enterprise_email']) : '' ?>" required>
+                        <input type="email" class="form-control <?php if (isset($errors['enterprise_email'])) echo 'is-invalid'; ?>" id="email" name="enterprise_email" placeholder="saisissez votre adresse mail" value="<?= isset($_POST['enterprise_email']) ? htmlspecialchars($_POST['enterprise_email']) : '' ?>" required>
 
                         <div class="invalid-feedback" id="emailValidationFeedback">
 
@@ -190,7 +190,7 @@
 
                         <div class="input-group d-flex">
 
-                            <input type="password" class="form-control rounded mt-1 password-input <?php if (isset($errors['enterprise_password'])) echo 'is-invalid'; ?>" name="enterprise_password" placeholder="Votre mot de passe" aria-label="password" aria-describedby="password" id="password-input">
+                            <input type="password" class="form-control rounded mt-1 password-input <?php if (isset($errors['enterprise_password'])) echo 'is-invalid'; ?>" name="enterprise_password" placeholder="********" aria-label="password" aria-describedby="password" id="password-input">
 
                             <div class="invalid-feedback" id="passwordValidationFeedback">
 
