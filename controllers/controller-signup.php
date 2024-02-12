d3a2507e608ea6ac7ce71669d8debc29faea8190
LN27100 <heloceinlovehotmail.fr>
2024-02-12 10:14:32 +0100

    rectif

--- controllers/controller-signup.php
+++ controllers/controller-signup.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["enterprise_siret"])) {
         $errors["enterprise_siret"] = "Champ obligatoire";
     } elseif (strlen($_POST["enterprise_siret"]) < 14) {
-        $errors["enterprise_siret"] = "Le siret doit contenir au moins 14 caractères";
+        $errors["enterprise_siret"] = "Le siret doit contenir 14 chiffres";
     } elseif (!is_numeric([0-9],$_POST["enterprise_siret"])) {
         $errors["enterprise_siret"] = "Le siret ne peut contenir que des chiffres";
     }
